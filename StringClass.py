b0df941f061927090780eee32ca0abc444fb519e
shrey-deloitte <shredeshpande@deloitte.com>
2022-04-05 13:29:44 +0530

    other classes and methods added

--- StringClass.py
+++ StringClass.py
@@ -1,22 +1,49 @@
-import string
+from itertools import permutations
+from collections import Counter
 
 
 class StringClass:
-    def __init__(self,str):
-        self.str=str
+    def __init__(self, inpt):
+        self.string = inpt
 
     def length(self):
-        print(len(self.str))
+        return len(self.string)
+
+    def tolist(self):
+        return list(self.string)
 
 
-    def stringToListChar(self):
+class SearchCommonElements(StringClass):
+    def commonElements(self):
+        d = dict(Counter(list(self.string)))
+        res = []
+        for j in d:
+            if d[j] >= 2:
+                res.append(j)
+        return res
 
-        self.str = self.str.split()
-        list1 = list(map(list, self.str))
-        print(list1)
+
+class PairsPossible(StringClass):
+
+    def pair(self):
+        self.perm = permutations(list(self.string))
+        li = []
+        for i in self.perm:
+            li.append(i)
+            print(list(i), end=" ")
+
+    def show(self):
+        return self.li
 
 
-str=StringClass("Hello")
-str.length()
-str.stringToListChar()
-
+print(" Enter your number string")
+userinput = input()
+objstringclass = StringClass(userinput)
+print("Length of string is: ", objstringclass.length())
+print("String converted to list: ", objstringclass.tolist())
+objpairpossible = PairsPossible(userinput)
+print("All possible pairs are: ")
+objpairpossible.pair()
+objsearchcommonelements = SearchCommonElements(userinput)
+print()
+print("Common elements are: ", objsearchcommonelements.commonElements())
