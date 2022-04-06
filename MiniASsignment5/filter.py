83d58287950b786ecccfe20caee67fbb48e53e65
shrey-deloitte <shredeshpande@deloitte.com>
2022-04-06 12:59:51 +0530

    MiniAssignment5 push

---
+++ MiniASsignment5/filter.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+lst1 = [-1000, 500, -600, 700, 5000, -90000, -17500]
+negNum = list(map(lambda x: -x, list(filter((lambda x: x < 0), lst1))))
+print(negNum)
+
+
+lst1 = [-1000, 500, -600, 700, 5000, -90000, -17500]
+print(reduce(lambda x, y: x * y, lst1))
+
+
+
+lst1 = ["Netflix", "Hulu", "Sling", "Hbo"]
+lst2 = [198, 166, 237, 125]
+res= dict(zip(lst1, lst2))
+print(dict(res))
+
+
--- MiniAssignment4/InbuiltFunc.py
+++ MiniAssignment4/InbuiltFunc.py
@@ -2,11 +2,10 @@ func = lambda a, b, c, x: a * x ** 2 + b * x + c
 
 print(func(1, 2, 3, 4))
 
-
 lst1 = ["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
 
-countOfa = list(map(lambda x:x.count("a"),lst1))
-countOfA = list(map(lambda x:x.count("A"),lst1))
+countOfa = list(map(lambda x: x.count("a"), lst1))
+countOfA = list(map(lambda x: x.count("A"), lst1))
 
 print(countOfa)
 print(countOfA)
